97c28829eaa18c1eadd6dcd7aa05c56664f323ae
A.Tumakov <a.tumakov@mfisoft.ru>
2019-02-05 11:46:22 +0300

    First homework commit

---
+++ csharp-example-1/csharp-example-1/UnitTest1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+
+namespace csharp_example_1
+{
+
+    [TestFixture]
+    public class UnitTest1
+    {
+        private IWebDriver driver;
+        private WebDriverWait wait;
+
+        [SetUp]
+        public void start()
+        {
+            driver = new ChromeDriver();
+            wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+        }
+
+        [Test]
+        public void TestMethod1()
+        {
+            driver.Url = "http://software-testing.ru";
+            wait.Until(ExpectedConditions.TitleIs("Software-Testing.Ru"));
+        }
+
+        [TearDown]
+        public void stop()
+        {
+            driver.Quit();
+            driver = null;
+        }
+    }
+}
