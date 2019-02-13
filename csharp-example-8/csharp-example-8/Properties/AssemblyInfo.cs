8d2fce486595b0e83a12c53210306f5bc750cc9f
A.Tumakov <a.tumakov@mfisoft.ru>
2019-02-13 16:23:09 +0300

    HomeWork-8

---
+++ csharp-example-8/csharp-example-8/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("csharp-example-8")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("csharp-example-8")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("a3b14016-b5d2-4f31-bf7d-9d6758db57b1")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ csharp-example-8/csharp-example-8/UnitTest1.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+
+namespace csharp_example_1
+{
+
+    [TestFixture]
+    public class StickersTest
+
+    {
+        private ChromeDriver driver;
+        private WebDriverWait wait;        
+
+        [SetUp]
+        public void start()
+        {
+            driver = new ChromeDriver();
+
+            driver.Manage().Window.Maximize();
+
+            wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+        }
+
+        [Test]
+        public void TestMethod1()
+        {
+            driver.Url = "http://localhost/litecart/en/";
+            
+            wait.Until(ExpectedConditions.TitleContains("Online Store | My Store"));
+
+            IWebElement topProducts = driver.FindElement(By.Id("box-most-popular"));
+            IWebElement centerProducts = driver.FindElement(By.Id("box-campaigns"));
+            IWebElement bottomProducts = driver.FindElement(By.Id("box-latest-products"));
+
+            IList<IWebElement> topListProducts = topProducts.FindElements(By.CssSelector("li"));
+
+            foreach (IWebElement el in topListProducts) {
+                bool trigger = false;
+                if (IsElementPresent(el, By.CssSelector("div.sticker.new")))
+                {
+                    trigger = true;
+                }
+                else if (IsElementPresent(el, By.CssSelector("div.sticker.sale")))
+                {
+                    trigger = true;
+                }
+                else {
+                    trigger = false;
+                }
+                Assert.IsTrue(trigger);
+                
+            }
+
+            IList<IWebElement> centerListProducts = centerProducts.FindElements(By.CssSelector("li"));
+            foreach (IWebElement el in centerListProducts)
+            {
+                bool trigger = false;
+                if (IsElementPresent(el, By.CssSelector("div.sticker.new")))
+                {
+                    trigger = true;
+                }
+                else if (IsElementPresent(el, By.CssSelector("div.sticker.sale")))
+                {
+                    trigger = true;
+                }
+                else
+                {
+                    trigger = false;
+                }
+                Assert.IsTrue(trigger);
+            }
+
+
+            IList<IWebElement> bottomListProducts = bottomProducts.FindElements(By.CssSelector("li"));
+            foreach (IWebElement el in bottomListProducts)
+            {
+                bool trigger = false;
+                if (IsElementPresent(el, By.CssSelector("div.sticker.new")))
+                {
+                    trigger = true;
+                }
+                else if (IsElementPresent(el, By.CssSelector("div.sticker.sale")))
+                {
+                    trigger = true;
+                }
+                else
+                {
+                    trigger = false;
+                }
+                Assert.IsTrue(trigger);
+            }
+
+        }
+
+
+
+        public bool IsElementPresent(IWebElement element, By locator)
+        {
+            try
+            {
+                element.FindElement(locator);
+                return true;
+            }
+            catch (NoSuchElementException ex)
+            {
+                return false;
+            }
+        }
+
+        public bool AreElementsPresent(IWebElement driver, By locator)
+        {
+            return driver.FindElements(locator).Count > 0;
+        }
+
+        [TearDown]
+        public void stop()
+        {
+            driver.Quit();
+            driver = null;
+
+        }
+    }
+}
