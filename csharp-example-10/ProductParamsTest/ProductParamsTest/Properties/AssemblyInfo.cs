0cbf7d88bcdcf0ed59a80fe50ea724ba02a5bf0e
A.Tumakov <a.tumakov@mfisoft.ru>
2019-02-18 12:25:53 +0300

    Update HomeWork_8

---
+++ csharp-example-10/ProductParamsTest/ProductParamsTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ProductParamsTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ProductParamsTest")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("99eb7ace-6219-4cc3-939b-9c458097d89b")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ csharp-example-10/ProductParamsTest/ProductParamsTest/UnitTest1.cs
@@ -0,0 +1,94 @@
+﻿using System;
+
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+
+namespace csharp_example_10
+{
+
+    [TestFixture]
+    public class ProductCheckParams
+
+    {
+        private ChromeDriver driver;
+        private WebDriverWait wait;
+       
+
+        [SetUp]
+        public void start()
+        {
+            driver = new ChromeDriver();
+
+            driver.Manage().Window.Maximize();
+
+            wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+        }
+
+        [Test]
+        public void TestMethod1()
+        {
+            driver.Url = "http://localhost:/litecart/admin/.";
+            wait.Until(ExpectedConditions.ElementToBeClickable(By.Name("login")));
+            driver.FindElement(By.Name("username")).SendKeys("admin");
+            driver.FindElement(By.Name("password")).SendKeys("admin");
+            driver.FindElement(By.Name("login")).Click();
+            wait.Until(ExpectedConditions.ElementToBeClickable(By.XPath("//*[@title='My Store']")));
+
+            int count = driver.FindElements(By.XPath("//ul[@id='box-apps-menu']/li")).Count;
+
+            for (int i = 1; i <= count; i++)
+            {
+                string locator = "//ul[@id='box-apps-menu']/li[" + i + "]";
+                wait.Until(ExpectedConditions.ElementIsVisible(By.XPath(locator)));
+                driver.FindElement(By.XPath(locator)).Click();
+
+                if (AreElementsPresent(driver, By.XPath(locator + "/ul")))
+                {
+
+                    int subCount = driver.FindElements(By.XPath(locator + "//li")).Count;
+
+                    for (int j = 1; j <= subCount; j++)
+                    {
+                        string sublocator = locator + "//li[" + j + "]";
+                        wait.Until(ExpectedConditions.ElementIsVisible(By.XPath(sublocator)));
+                        driver.FindElement(By.XPath(sublocator)).Click();
+
+                        wait.Until(ExpectedConditions.ElementIsVisible(By.Id("content")));
+                        Assert.IsTrue(IsElementPresent(driver, By.CssSelector("h1")));
+
+                    }
+
+                }
+            }
+
+        }
+
+        public bool IsElementPresent(IWebDriver driver, By locator)
+        {
+            try
+            {
+                driver.FindElement(locator);
+                return true;
+            }
+            catch (NoSuchElementException ex)
+            {
+                return false;
+            }
+        }
+
+        public bool AreElementsPresent(IWebDriver driver, By locator)
+        {
+            return driver.FindElements(locator).Count > 0;
+        }
+
+        [TearDown]
+        public void stop()
+        {
+            driver.Quit();
+            driver = null;
+
+        }
+    }
+}
