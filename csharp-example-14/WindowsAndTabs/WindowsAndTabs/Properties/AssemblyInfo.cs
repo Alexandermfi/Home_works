93b3df0ea886524cef64c4141f0dd6833866b04e
A.Tumakov <a.tumakov@mfisoft.ru>
2019-02-27 12:42:51 +0300

    HomeWork_example_14

---
+++ csharp-example-14/WindowsAndTabs/WindowsAndTabs/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("WindowsAndTabs")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("WindowsAndTabs")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("8827b950-b5e0-4552-baf9-ed9a91350927")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ csharp-example-14/WindowsAndTabs/WindowsAndTabs/UnitTest1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.IE;
+using OpenQA.Selenium.Support.UI;
+
+namespace csharp_example_1
+{
+
+    [TestFixture]
+    public class CountryScenario
+
+    {
+        private IWebDriver driver;
+        private WebDriverWait wait;
+
+        [SetUp]
+        public void start()
+        {
+            driver = new ChromeDriver();
+            driver.Manage().Window.Maximize();
+            wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+        }
+        [Test]
+        public void TestMethod1()
+        {
+            driver.Url = "http://localhost:/litecart/admin/.";
+            wait.Until(ExpectedConditions.ElementToBeClickable(By.Name("login")));
+            driver.FindElement(By.Name("username")).SendKeys("admin");
+            driver.FindElement(By.Name("password")).SendKeys("admin");
+            driver.FindElement(By.Name("login")).Click();
+            wait.Until(ExpectedConditions.ElementToBeClickable(By.XPath("//*[@title='My Store']")));
+
+
+            driver.FindElement(By.XPath("//span[contains(.,'Countries')]")).Click();
+            wait.Until(ExpectedConditions.ElementToBeClickable(By.Id("content")));
+
+            IList<IWebElement> Rows = driver.FindElements(By.CssSelector("td:nth-child(5) a")); 
+
+            Rows[new Random().Next(0, Rows.Count)].Click();
+
+            wait.Until(ExpectedConditions.ElementIsVisible(By.XPath("//td[@id='content']")));
+
+            IList<IWebElement> links = driver.FindElements(By.XPath("//i[@class='fa fa-external-link']/parent::*"));
+            string mainWindow = driver.CurrentWindowHandle;
+
+            foreach (IWebElement el in links) {
+                el.Click();                   
+                List<string> windowHandles = driver.WindowHandles.ToList();
+                foreach (string handle in windowHandles) {
+                    if (mainWindow != handle) {                       
+                        driver.SwitchTo().Window(handle).Close();                  
+                        driver.SwitchTo().Window(mainWindow);
+                    }
+
+                }
+               
+               
+            }
+            
+        }  
+   
+
+        [TearDown]
+        public void stop()
+        {
+            driver.Quit();
+            driver = null;
+
+        }
+    }
+}
